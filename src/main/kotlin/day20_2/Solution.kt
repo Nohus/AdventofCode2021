0fd94ff5a96fb118d8d088ca21467c91f2989488
Nohus <git@nohus.eu>
2021-12-20 22:24:33 +0000

    Day 20

---
+++ src/main/kotlin/day20_2/Solution.kt
@@ -0,0 +1,20 @@
+package day20_2
+
+import solve
+import utils.Point
+
+fun main() {
+    solve { lines ->
+        val algorithm = lines.first().map { it == '#' }
+        val input = lines.drop(2).flatMapIndexed { y, line -> line.mapIndexed { x, char -> Point(x, y) to (char == '#') } }.toMap().withDefault { false }
+        (0 until 50).fold(input) { image, _ ->
+            val new = mutableMapOf<Point, Boolean>()
+            val range = -2..(image.keys.maxOf { it.x } + 2)
+            for (y in range) for (x in range) {
+                val kernel = mutableListOf<Point>().also { for (yDelta in -1..1) for (xDelta in -1..1) it += Point(x + xDelta, y + yDelta) }
+                new[Point(x + 2, y + 2)] = algorithm[kernel.joinToString("") { if (image.getValue(it)) "1" else "0" }.toInt(2)]
+            }
+            new.withDefault { new.getValue(Point(0, 0)) }
+        }.count { it.value }
+    }
+}
